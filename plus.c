bf52ebc144f5751f24345f077b2de822c676d1bc
LZH <1298381995@qq.com>
2024-09-04 21:29:37 +0800

    测4

---
+++ plus.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a=0;
+    int b=0;
+    printf("please give me two numbers,and there must be something between them");
+    scanf("%d %d",&a,&b);
+    printf("%d+%d=%d",a,b,a+b);
+    return 0;
+}
